a89dc432557ecfbe549044c841a188d901df4973
AngelF9 <112910128+AngelF9@users.noreply.github.com>
2023-05-23 20:40:54 -0700

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,15 @@
+// while.cpp
+// Name:Angel Fuentes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 3;
+  while (i < 100) {
+    cout << i << endl;
+    i += 3;
+  }
+
+  return 0;
+}
